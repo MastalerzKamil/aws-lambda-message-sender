05b8811aff7c3f461d557994ceb9badc40c48da0
Kamil Mastalerz <kamil.mastalerz@netguru.com>
2021-03-08 16:22:22 +0100

    gs

--- handler.js
+++ handler.js
@@ -1,4 +1,11 @@
+const randomBytes = require('crypto').randomBytes;
 const Messenger = require('./messenger.js');
+
+const AWS = require('aws-sdk');
+
+const ddb = new AWS.DynamoDB.DocumentClient();
+
+
 
 const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
@@ -6,15 +13,24 @@ const twilioClient = require('twilio')(twilioAccountSid, twilioAuthToken); // es
 
 module.exports.sendText = (event, context, callback) => {
   const messenger = new Messenger(twilioClient);
+  const userId = toUrlString(randomBytes(16));
 
   const response = {
     headers: { 'Access-Control-Allow-Origin': '*' }, // CORS requirement
     statusCode: 200,
+    body: JSON.stringify({
+      UserId: userId,
+      User: event.body.username,
+      To: event.body.to,
+      message: event.body.message,
+      RequestTime: new Date().toISOString(),
+    })
   };
 
   Object.assign(event, { from: process.env.TWILIO_PHONE_NUMBER });
 
   messenger.send(event)
+  .then(() => saveUser(userId, event.body.username, event.body.to))
   .then((message) => {
     // text message sent! ✅
     console.log(`message ${message.body}`);
@@ -34,3 +50,22 @@ module.exports.sendText = (event, context, callback) => {
     callback(null, response);
   });
 };
+
+function saveUser(userId, username, phoneNumber) {
+  return ddb.put({
+      TableName: 'users-table-dev',
+      Item: {
+          UserId: userId,
+          User: username,
+          PhoneNumber: phoneNumber,
+          RequestTime: new Date().toISOString(),
+      },
+  }).promise();
+}
+
+function toUrlString(buffer) {
+  return buffer.toString('base64')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_')
+      .replace(/=/g, '');
+}
